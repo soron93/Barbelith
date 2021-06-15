fe01fa6f5fa69274d7cc7baad999b98b3c84d22f
beta <beta@kusarigama.local>
2021-06-15 10:22:25 +0200

    deleted images and went back to basics using shapes on canvas. added ball that moves with arrow keys and added start screen

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let canvas = document.getElementById('myCanvas'); // grabs the canvas
 let ctx = canvas.getContext('2d');
-// canvas.style.background = 'red' // sets canvas background color
+canvas.style.background = 'yellow' // sets canvas background color
 canvas.style.border = '2px solid black'; // sets canvas border
 // bg.src = './images/bg.png'
 
 
 // load all images
 let bg = new Image();
-bg.src = './images/bg.png';
+
+
 bg.src = './images/skyscraper2.jpeg';
+
+let player1 = new Image();
+player1.src = '/images/jumpystill.bmp'
+
+
+
+
+
+
+
+
 
 
 // paintbrush
@@ -31,51 +43,6 @@ let intervalId = 0
 
 // function draw()
 // ctx.drawImage
-
-
-
-//Basic Animation Template 
-function draw() {
-
-    ctx.drawImage(bg, 0, 0)
-
-
-    if (gameOver) {
-        cancelAnimationFrame(intervalId)
-    }
-    else {
-        intervalId = cancelAnimationFrame(draw)
-    }
-
-}
-
-
-function start() {
-    canvas.style.display = 'block'
-    restartBtn.style.display = 'none'
-    startBtn.style.display = 'none'
-    draw()
-}
-
-
-
-// All even funciton wll begin in the windows event load listener
-window.addEventListener('load', () => {
-
-    canvas.style.display = 'none' // hides canvas
-    restartBtn.style.display = 'none' // hide restart button
-
-    startBtn.addEventListener('click', () => {
-        start()
-
-    })
-
-    restartBtn.addEventListener('click', () => {
-
-    })
-})
-
-
 
 // function animate() {
 //     if (gameOver) {
